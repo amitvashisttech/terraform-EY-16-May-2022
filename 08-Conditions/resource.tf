feb2afe5b464c9ac43db0a28631011af6a1cbc2d
Amit Vashist <amitvashist7@outlook.com>
2022-05-17 07:08:32 +0000

    08-Conditions

---
+++ 08-Conditions/resource.tf
@@ -0,0 +1,14 @@
+# Create a resource group
+resource "azurerm_resource_group" "frontend" {
+  count    = var.multi-region-deployment ? 3 : 1
+  name     = "${var.my_rg_name}-${count.index}"
+  location = var.my_loc[count.index]
+
+tags = { 
+   Owner = "Amit Vashist"
+   Team  = "DevOps Team"
+   Project = "Terraform Automation" 
+ 
+ }
+ 
+}
